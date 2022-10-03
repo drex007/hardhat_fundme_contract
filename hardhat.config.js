19b63a5eb8f7c25838e595994929cc10053502a4
drex007 <vykeoj@gmail.com>
2022-10-03 12:22:31 +0100

    feat: comment to hardhatconfig file

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,7 +16,7 @@ module.exports = {
       accounts: []
     }
   },
-  namedAccounts: {
+  namedAccounts: { // This namedAccount objecct is necessary inorder to allow harhat choose which of the address to use 
     deployer: {
         default: 0, // here this will by default take the first account as deployer
         1: 0, // similarly on mainnet or testnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
